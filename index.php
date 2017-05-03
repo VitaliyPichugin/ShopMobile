67432482a92860804e14ac801cb24847c9a7598d
VitaliyPichugin <vexel1992@gmail.com>
2017-05-03 20:22:11 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+//запрет прямого обращенгия
+define('ISHOP', true);
+
+//подключение файла конфигурации
+require_once 'config.php';
+
+//подключение контроллера
+require_once CONTROLLER;
+?>
